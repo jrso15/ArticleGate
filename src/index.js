cf5a4faf01694767cc04066b108910259b762d13
jrso15 <janilleolegario@gmail.com>
2021-06-09 01:46:01 +0800

    remove console

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
 import RapplerModal from "../Classes/RapplerModal.js";
 
 global.RapplerModal = RapplerModal;
-
-// import NormalModal from "/src/Components/NormalModal/NormalModal.js";
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// ReactDOM.render(<NormalModal />, document.body);
-
-// // import PlusModal from "/src/Components/PlusModal/PlusModal.js";
-
-// // import React from "react";
-// // import ReactDOM from "react-dom";
-
-// // ReactDOM.render(<PlusModal />, document.body);
-
-// // import MoveModal from "/src/Components/MoveModal/MoveModal.js";
-
-// // import React from "react";
-// // import ReactDOM from "react-dom";
-
-// // ReactDOM.render(<MoveModal />, document.body);
